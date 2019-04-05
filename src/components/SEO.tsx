2fb59015d4cc04d9c3b9a635a81c8370f3f6da32
Dylan Tackoor <dylan@worldmedia.net>
2019-04-05 11:57:06 -0400

    open graph tags now use full url

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -2,7 +2,7 @@ import { graphql, useStaticQuery } from 'gatsby'
 import React, { FunctionComponent } from 'react'
 import Helmet from 'react-helmet'
 
-const graphImg = require('../images/opengraph.png')
+const graphImgRelPath = require('../images/opengraph.png')
 
 interface ISeoConfig {
 	description?: string
@@ -29,6 +29,7 @@ export const SEO: FunctionComponent<ISeoConfig> = props => {
 	)
 
 	const siteMetadata: ISeoConfig = site.siteMetadata
+	const graphImgURL = `${siteMetadata.siteUrl}${graphImgRelPath}`
 	const title = props.title
 		? `${props.title} | ${siteMetadata.title}`
 		: siteMetadata.title
@@ -46,8 +47,8 @@ export const SEO: FunctionComponent<ISeoConfig> = props => {
 			<meta property='og:description' content={description} />
 			<meta property='og:url' content={siteMetadata.siteUrl} />
 			<meta property='og:site_name' content={title} />
-			<meta property='og:image' content={graphImg} />
-			<meta property='og:image:secure_url' content={graphImg} />
+			<meta property='og:image' content={graphImgURL} />
+			<meta property='og:image:secure_url' content={graphImgURL} />
 			<meta property='og:image:width' content='1200' />
 			<meta property='og:image:height' content='630' />
 
@@ -57,10 +58,7 @@ export const SEO: FunctionComponent<ISeoConfig> = props => {
 			<meta name='twitter:description' content={description} />
 			<meta name='twitter:site' content='@worldmediamiami' />
 			<meta name='twitter:creator' content='@dylantackoor' />
-			<meta
-				name='twitter:image:src'
-				content={`${siteMetadata.siteUrl}${graphImg}`}
-			/>
+			<meta name='twitter:image:src' content={graphImgURL} />
 			<meta name='twitter:image:alt' content={title} />
 			<meta name='twitter:image:width' content='1200' />
 			<meta name='twitter:image:height' content='630' />
