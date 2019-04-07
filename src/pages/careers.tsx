351dea9d2d78b5263478c2f988723b9ebdb774bf
Dylan Tackoor <dylan@worldmedia.net>
2019-04-07 13:13:05 -0400

    careers page lists opening

--- src/pages/careers.tsx
+++ src/pages/careers.tsx
@@ -2,10 +2,26 @@ import React, { FunctionComponent } from 'react'
 import { Layout } from '../components/Layout'
 import { SEO } from '../components/SEO'
 
+import data from '../cms/content/careers.json'
+
 const CareersPage: FunctionComponent = () => (
 	<Layout>
 		<SEO title='Careers' />
-		<h1>Careers</h1>
+		<h1>{data.title}</h1>
+		<p>{data.intro}</p>
+
+		{data.departments.map(department => (
+			<div>
+				<h2>{department.name}</h2>
+				<ul>
+					{department.careers.map(career => (
+						<li key={career.title}>
+							{career.title} | {career.opened}
+						</li>
+					))}
+				</ul>
+			</div>
+		))}
 	</Layout>
 )
 
