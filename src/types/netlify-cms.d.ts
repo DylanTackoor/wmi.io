56c03ab4eaf656bfb52aa22f82258afdf3e36472
Dylan Tackoor <dylan@worldmedia.net>
2019-04-07 10:56:55 -0400

    added initial netlify-cms types

--- src/types/netlify-cms.d.ts
+++ src/types/netlify-cms.d.ts
@@ -1,3 +1,42 @@
+import { Component } from 'react'
+
 export = CMS
 
-declare const CMS: any
+interface IEditorComponentOptions {
+	/** Internal id of the component */
+	id: string
+
+	/** Visible label */
+	label: string
+
+	/** Fields the user need to fill out when adding an instance of the component */
+	fields: Array<{ name: string; label: string; widget: string }>
+
+	/**
+	 * Preview output for this component. Can either be a string or a React component
+	 * (component gives better render performance)
+	 */
+	toPreview: (obj: string | Component) => string
+
+	/** Pattern to identify a block as being an instance of this component */
+	pattern: RegExp
+
+	/** Function to extract data elements from the regexp match */
+	fromBlock: (match: any) => any
+
+	/** Function to create a text block from an instance of this component */
+	toBlock: (obj: any) => any
+}
+
+declare const CMS: {
+	registerWidget: (
+		/** 1 */
+		name: string,
+		/** 2 */
+		control: Component | string,
+		/** 3 */
+		preview?: Component
+	) => any
+
+	registerEditorComponent: (config: IEditorComponentOptions) => any
+}
