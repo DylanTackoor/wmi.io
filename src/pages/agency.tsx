b9ff674737b889b9ed7e39ad3e187cea767ddbd6
Dylan Tackoor <dylan@worldmedia.net>
2019-04-11 10:15:29 -0400

    agenyc page immediately returns

--- src/pages/agency.tsx
+++ src/pages/agency.tsx
@@ -6,61 +6,59 @@ import { SEO } from '../components/SEO'
 
 import agency from '../cms/content/agency.json'
 
-const AgencyPage: FunctionComponent = () => {
-	return (
-		<Layout>
-			<SEO />
-			<WorldmediaLD />
+const AgencyPage: FunctionComponent = () => (
+	<Layout>
+		<SEO />
+		<WorldmediaLD />
 
-			<header>
-				<h1>{agency.title}</h1>
-			</header>
+		<header>
+			<h1>{agency.title}</h1>
+		</header>
 
-			<div>
-				<p>{agency.introText}</p>
-				<h2>{agency.introTitle}</h2>
-			</div>
+		<div>
+			<p>{agency.introText}</p>
+			<h2>{agency.introTitle}</h2>
+		</div>
 
-			<div>
-				<h2>{agency.teamTitle}</h2>
-				{agency.skills.map(skill => (
-					<p key={skill}>{skill}</p>
-				))}
-			</div>
+		<div>
+			<h2>{agency.teamTitle}</h2>
+			{agency.skills.map(skill => (
+				<p key={skill}>{skill}</p>
+			))}
+		</div>
 
-			<div>
-				<h2>{agency.statsTitle}</h2>
-				<p>{agency.statsSubtitle}</p>
-				{agency.stats.map(stat => (
-					<div key={stat.description}>
-						<p>{stat.number}</p>
-						<p>{stat.stat}</p>
-						<p>{stat.description}</p>
-					</div>
-				))}
-			</div>
+		<div>
+			<h2>{agency.statsTitle}</h2>
+			<p>{agency.statsSubtitle}</p>
+			{agency.stats.map(stat => (
+				<div key={stat.description}>
+					<p>{stat.number}</p>
+					<p>{stat.stat}</p>
+					<p>{stat.description}</p>
+				</div>
+			))}
+		</div>
 
-			<div>
-				<h2>Our Offices</h2>
-				{agency.ourOffices.map(office => (
-					<div key={office.city}>
-						<img src={office.logo} />
-						<p>{office.city}</p>
-						<p>{office.title}</p>
-					</div>
-				))}
+		<div>
+			<h2>Our Offices</h2>
+			{agency.ourOffices.map(office => (
+				<div key={office.city}>
+					<img src={office.logo} />
+					<p>{office.city}</p>
+					<p>{office.title}</p>
+				</div>
+			))}
 
-				<h2>Partner Offices</h2>
-				{agency.partnerOffices.map(office => (
-					<div key={office.city}>
-						<img src={office.logo} />
-						<p>{office.city}</p>
-						<p>{office.title}</p>
-					</div>
-				))}
-			</div>
-		</Layout>
-	)
-}
+			<h2>Partner Offices</h2>
+			{agency.partnerOffices.map(office => (
+				<div key={office.city}>
+					<img src={office.logo} />
+					<p>{office.city}</p>
+					<p>{office.title}</p>
+				</div>
+			))}
+		</div>
+	</Layout>
+)
 
 export default AgencyPage
